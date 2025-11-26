fa5aff999fd79719429c014457c086a25566bb53
Daniil Havrylov <daniil.gavrilov.10@gmail.com>
2025-11-26 14:54:07 +0000

    refactor: migrate from Playwright to Selenium

--- services/driver.py
+++ services/driver.py
@@ -1,23 +1,37 @@
-from playwright.async_api import async_playwright, Page
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
 
 class Driver:
     def __init__(self):
-        self.playwright = None
         self.browser = None
 
-    async def start(self):
-        self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.chromium.launch(headless=False)
+    def start(self):
+        chrome_options = Options()
+        chrome_options.add_argument("--lang=en-US")
+        chrome_options.add_argument("--window-size=800,800")
+        chrome_options.add_argument("--disable-infobars")
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--disable-extensions")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_prefs = {
+            "profile.default_content_setting_values.media_stream_mic": 1,
+            "profile.default_content_setting_values.media_stream_camera": 1,
+            "profile.default_content_setting_values.notifications": 2
+        }
+        chrome_options.add_experimental_option("prefs", chrome_prefs)
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
-    async def get_page(self, link) -> Page:
+        self.browser = webdriver.Chrome(options=chrome_options)
+
+    def get_page(self, link):
         if not self.browser:
-            raise Exception("Browser not started. Call 'await start()' first.")
-        page = await self.browser.new_page()
-        await page.goto(link)
-        return page
+            raise Exception("Browser not started. Call 'start()' first.")
 
-    async def close(self):
+        self.browser.get(link)
+        return self.browser
+
+    def close(self):
         if self.browser:
-            await self.browser.close()
-        if self.playwright:
-            await self.playwright.stop()
+            self.browser.quit()
